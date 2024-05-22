f5a6e8d81073d582e5fb5723e2e245e7e2f0ce40
nevernever69 <nevernevernever69@protonmail.com>
2024-05-22 08:02:33 +0530

    Changed Title

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://debugoist.nevernever.me/",
   integrations: [tailwind(), icon()],
 });
